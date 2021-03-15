cc654657ea97125a383a8568bc278df05db87399
Nathan <56935011+Zubinatub@users.noreply.github.com>
2021-03-14 22:50:24 -0400

    now plays background music

--- LeagueWindows/Scene.cpp
+++ LeagueWindows/Scene.cpp
@@ -4,7 +4,21 @@
 #include "Scene.hpp"
 
 Scene::Scene(){
-
+	int i, count = SDL_GetNumAudioDevices(0);
+	if (count < 1) {
+		SDL_Log("Audio error. %s", SDL_GetError());
+	}
+	for (i = 0; i < count; ++i) {
+		SDL_Log("Audio device %d: %s", i, SDL_GetAudioDeviceName(i, 0));
+	}
+	SDL_AudioSpec wavSpec;
+	Uint32 length;
+	Uint8* buffer;
+	SDL_LoadWAV("../assets/song.wav", &wavSpec, &buffer, &length);
+	SDL_AudioDeviceID device = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+	int success = SDL_QueueAudio(device, buffer, length);
+	SDL_Log("Audio success: %d %s", success, SDL_GetError());
+	SDL_PauseAudioDevice(device, 0);
 }
 
 Scene::~Scene(){
